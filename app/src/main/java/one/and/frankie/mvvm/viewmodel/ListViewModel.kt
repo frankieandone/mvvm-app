294ee3fd99330c1ad4393dcbd96e6820d1de8c70
Frankie <frankie.and.one@protonmail.com>
2019-05-25 00:18:08 +0100

    Finished S2.6.

--- app/src/main/java/one/and/frankie/mvvm/viewmodel/ListViewModel.kt
+++ app/src/main/java/one/and/frankie/mvvm/viewmodel/ListViewModel.kt
@@ -1,5 +1,31 @@
 package one.and.frankie.mvvm.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import one.and.frankie.mvvm.model.Country
 
-class ListViewModel: ViewModel()
+class ListViewModel: ViewModel() {
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("Afghanistan"),
+            Country("Albania"),
+            Country("Algeria"),
+            Country("Andorra"),
+            Country("Angola"),
+            Country("Antigua and Barbuda"),
+            Country("Argentina")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+}

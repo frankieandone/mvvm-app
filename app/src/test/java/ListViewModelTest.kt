f8d2ef2a98145c99640ea1b49b760c6421b9cf81
Frankie <frankie.and.one@protonmail.com>
2019-05-25 14:26:21 +0100

    Finished S2.22.

--- app/src/test/java/ListViewModelTest.kt
+++ app/src/test/java/ListViewModelTest.kt
@@ -1,16 +1,51 @@
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import one.and.frankie.mvvm.model.CountriesService
+import one.and.frankie.mvvm.model.Country
+import one.and.frankie.mvvm.viewmodel.ListViewModel
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
 class ListViewModelTest {
     @get:Rule var rule = InstantTaskExecutorRule()
+
+    @Mock lateinit var countriesService: CountriesService
+
+    @InjectMocks var listViewModel = ListViewModel()
+
+    private var testSingle: Single<List<Country>>? = null
+
+    @Before fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test fun getCountriesSuccess() {
+        val country = Country("countryName", "countryCapital", "url")
+        val countriesList = arrayListOf(country)
+
+        testSingle = Single.just(countriesList)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.countries.value?.size)
+        Assert.assertEquals(false, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
 
     @Before fun setUpRxSchedulers() {
         val immediate = object : Scheduler() {

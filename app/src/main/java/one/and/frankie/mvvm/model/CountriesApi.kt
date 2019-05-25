187e9e856f14480a76953c3e00ccf15b93d9b3a1
Frankie <frankie.and.one@protonmail.com>
2019-05-25 11:46:01 +0100

    Finished S2.11.

---
+++ app/src/main/java/one/and/frankie/mvvm/model/CountriesApi.kt
@@ -0,0 +1,9 @@
+package one.and.frankie.mvvm.model
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface CountriesApi {
+    @GET("DevTides/countries/master/countriesV2.json")
+    fun getCountries(): Single<List<Country>>
+}
